cd56919f7a1ad416bc3e931ca3ab496c67c47506
Michael Morrison <morri909@umn.edu>
2021-03-03 18:39:44 -0600

    Fix integration test

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,27 +1,35 @@
 package guru.springframework.repositories;
 
+import guru.springframework.bootstrap.RecipeBootstrap;
 import guru.springframework.domain.UnitOfMeasure;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private RecipeRepository recipeRepository;
 	@Autowired
 	private UnitOfMeasureRepository sut;
 
 	@Before
 	public void setUp() throws Exception {
+		recipeRepository.deleteAll();
+		categoryRepository.deleteAll();
+		sut.deleteAll();
+		RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, sut);
+		recipeBootstrap.onApplicationEvent(null);
 	}
 
 	@Test
